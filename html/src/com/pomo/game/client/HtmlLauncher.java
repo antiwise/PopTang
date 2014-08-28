1cc2ef1438fbbb0a58a2a74f9c4541a7d5252f61
Mr.Po <ldd_live@foxmail.com>
2014-08-28 16:12:30 +0800

    PopTang 项目初始化

---
+++ html/src/com/pomo/game/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.pomo.game.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.pomo.game.PopTangGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new PopTangGame();
+        }
+}
